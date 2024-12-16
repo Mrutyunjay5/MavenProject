90b7e3a800ae8c1fedee65e8288f2ace34335ede
Mrutyunjay Dravid <mrutyunjay.dravid@splus.local>
2024-12-16 14:45:08 +0530

    launch browser

--- src/main/java/launchBrowser/Chrome.java
+++ src/main/java/launchBrowser/Chrome.java
@@ -6,17 +6,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Chrome {
 
 	public static void main(String[] args) {
-		print();
-		
+		launchChrome();
+
 	}
-	
+
 	public static void launchChrome() {
-WebDriver driver = new ChromeDriver();
-		
+		WebDriver driver = new ChromeDriver();
+
 		driver.get("https://www.youtube.com/watch?v=FX322RVNGj4&t=6560s");
-		
+
 	}
-	
+
 	public static void print() {
 		System.out.println("print statement -------******--------");
 	}
---
+++ src/main/java/launchBrowser/ImportJar.java
@@ -0,0 +1,10 @@
+package launchBrowser;
+
+
+public class ImportJar {
+	
+	public static void main(String[] args) {
+		System.out.println(Chrome.class.getTypeName());
+	}
+
+}
