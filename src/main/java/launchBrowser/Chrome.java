16ad754bceb2dc8863f41c3386e57048eb36453e
Mrutyunjay Dravid <mrutyunjay.dravid@splus.local>
2024-12-16 14:02:37 +0530

    changed to display print statement

--- src/main/java/launchBrowser/Chrome.java
+++ src/main/java/launchBrowser/Chrome.java
@@ -6,10 +6,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Chrome {
 
 	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
+		print();
+		
+	}
+	
+	public static void launchChrome() {
+WebDriver driver = new ChromeDriver();
 		
 		driver.get("https://www.youtube.com/watch?v=FX322RVNGj4&t=6560s");
 		
-		
+	}
+	
+	public static void print() {
+		System.out.println("print statement -------******--------");
 	}
 }
